bdc4441175f4d51a058740fea14a9d02bd0dc2c8
arjaycuanan <cuananarjay.ca@gmail.com>
2026-06-13 17:28:13 +0800

    Added navbar, footer, globaljs, App vue template

--- airline-booking-system/client/src/App.vue
+++ airline-booking-system/client/src/App.vue
@@ -1,13 +1,19 @@
-<script >
-	
+<script setup>
+	import { RouterLink, RouterView } from 'vue-router';
+  import { useGlobalStore } from './stores/global';
+  import NavbarComponent from './components/NavbarComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
+
+  const globalStore = useGlobalStore();
 </script>
 
 <template>
-	
-
-	<main>
-      <router-view />
-  </main>
-
+  <div>
+    <NavbarComponent />
+      <main>
+          <RouterView />
+      </main>
+    <FooterComponent />
+  </div>
   
 </template>
